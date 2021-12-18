f30e2cee172a4f4656e89bb94bbe4c2825c2e193
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-12-18 12:39:05 -0500

    expand student model data

--- database/models/Student.js
+++ database/models/Student.js
@@ -5,12 +5,30 @@ const Student = db.define("student", {
 
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {notEmpty: true}
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {notEmpty: true}
+  }
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {notEmpty: true}
+  }
+
+  imageURL: {
+    type: Sequelize.STRING,
+    defaultValue: "https://it.tufts.edu/sites/default/files/inline-images/student-icon_1_2.png"
+  }
+
+  gpa: {
+    type: Sequelize.FLOAT(4,3),
+    validate: {max: 4, min: 0}
   }
 
 });
