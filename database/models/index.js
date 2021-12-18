acd1f5df5a159608050197ca4d8b9104cedf1f0a
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-12-18 12:43:37 -0500

    comment

--- database/models/index.js
+++ database/models/index.js
@@ -3,7 +3,9 @@
 const Student   = require('./Student');
 const Campus    = require('./Campus');
 
+// Students can be associated with at most one campus
 Student.belongsTo(Campus);
+// Campuses can be associated with many students
 Campus.hasMany(Student);
 
 module.exports = {
