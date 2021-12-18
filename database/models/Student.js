e9e4359db7eec998a3b2c6232af102f273238824
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-12-18 12:40:38 -0500

    comment

--- database/models/Student.js
+++ database/models/Student.js
@@ -3,6 +3,7 @@ const db = require('../db');
 
 const Student = db.define("student", {
 
+  // Name and email: all strings, neither Null nor empty.
   firstname: {
     type: Sequelize.STRING,
     allowNull: false,
@@ -21,11 +22,13 @@ const Student = db.define("student", {
     validate: {notEmpty: true}
   }
 
+  // Link to default student icon
   imageURL: {
     type: Sequelize.STRING,
     defaultValue: "https://it.tufts.edu/sites/default/files/inline-images/student-icon_1_2.png"
   }
 
+  // Decimal value on [0,4]. Rounds to 3 decimal places.
   gpa: {
     type: Sequelize.FLOAT(4,3),
     validate: {max: 4, min: 0}
