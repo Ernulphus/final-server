8be846434ec96abd79cc43f23b1b7d9eb25698bc
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-12-18 13:16:51 -0500

    give addresses to seed campi

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,11 +3,13 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
-		description: "This is a school in NYC."
+		description: "This is a school in NYC.",
+		address: "68th st and Lexington, New York"
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
-		description: "This is a school in MA."
+		description: "This is a school in MA.",
+		address: "Cambridge, MA"
 	});
 
 	const dummy_student = await Student.create({
@@ -16,7 +18,7 @@ const seedDB = async () => {
 		});
 
 	await dummy_student.setCampus(dummy_campus);
-	
+
 }
 
 module.exports = seedDB;
